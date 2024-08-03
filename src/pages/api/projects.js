0f66bb8fe6a41b962c38b78b283deec2c1e46eb5
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2024-08-02 17:15:41 -0700

    Updated fetchProjects to use environment variable for backend URL

--- src/pages/api/projects.js
+++ src/pages/api/projects.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const fetchProjects = async () => {
   const response = await axios.get(
-    'https://martha.codes/webycare/wp-json/wp/v2/project'
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/wp-json/wp/v2/project`
   )
   return response.data
 }
